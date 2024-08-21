dc35827803d99c447adabf24b2ee50e2814f8678
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-21 12:19:30 -0900

    Update exam.controller.ts

--- src/exam.controller.ts
+++ src/exam.controller.ts
@@ -4,7 +4,7 @@ import { ExamService } from './exam.service';
 @Controller()
 export class ExamController {
   constructor(private readonly appService: ExamService) {}
-
+  
   @Get()
   getHello(): string {
     return this.examService.getHello();
